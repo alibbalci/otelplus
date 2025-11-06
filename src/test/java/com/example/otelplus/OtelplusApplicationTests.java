cdadb1ddfeda5ff54bdf63ef502c0eb94547ecad
Muhammet Ali Balcı <aliumut2736@gmail.com>
2025-11-06 19:42:49 +0300

    Proje Oluşturuldu

---
+++ src/test/java/com/example/otelplus/OtelplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otelplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
